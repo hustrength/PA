e82bd41f973a91b0bfd6a07772339d405f201183
hustrength <hustrength2017@gmail.com>
2021-01-03 02:21:26 +0800

    PA2.1 done

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -1,76 +1,77 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov) {
-  operand_write(id_dest, &id_src->val);
-  print_asm_template2(mov);
+        operand_write(id_dest, &id_src->val);
+        print_asm_template2(mov);
 }
 
 make_EHelper(push) {
-  TODO();
+        /* PA2.1 */
+        rtl_push(&id_dest->val);
 
-  print_asm_template1(push);
+        print_asm_template1(push);
 }
 
 make_EHelper(pop) {
-  TODO();
+        TODO();
 
-  print_asm_template1(pop);
+        print_asm_template1(pop);
 }
 
 make_EHelper(pusha) {
-  TODO();
+        TODO();
 
-  print_asm("pusha");
+        print_asm("pusha");
 }
 
 make_EHelper(popa) {
-  TODO();
+        TODO();
 
-  print_asm("popa");
+        print_asm("popa");
 }
 
 make_EHelper(leave) {
-  TODO();
+        TODO();
 
-  print_asm("leave");
+        print_asm("leave");
 }
 
 make_EHelper(cltd) {
-  if (decinfo.isa.is_operand_size_16) {
-    TODO();
-  }
-  else {
-    TODO();
-  }
+        if (decinfo.isa.is_operand_size_16) {
+            TODO();
+        }
+        else {
+            TODO();
+        }
 
-  print_asm(decinfo.isa.is_operand_size_16 ? "cwtl" : "cltd");
+        print_asm(decinfo.isa.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
 make_EHelper(cwtl) {
-  if (decinfo.isa.is_operand_size_16) {
-    TODO();
-  }
-  else {
-    TODO();
-  }
+        if (decinfo.isa.is_operand_size_16) {
+            TODO();
+        }
+        else {
+            TODO();
+        }
 
-  print_asm(decinfo.isa.is_operand_size_16 ? "cbtw" : "cwtl");
+        print_asm(decinfo.isa.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
 make_EHelper(movsx) {
-  id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
-  rtl_sext(&s0, &id_src->val, id_src->width);
-  operand_write(id_dest, &s0);
-  print_asm_template2(movsx);
+        id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
+        rtl_sext(&s0, &id_src->val, id_src->width);
+        operand_write(id_dest, &s0);
+        print_asm_template2(movsx);
 }
 
 make_EHelper(movzx) {
-  id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
-  operand_write(id_dest, &id_src->val);
-  print_asm_template2(movzx);
+        id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
+        operand_write(id_dest, &id_src->val);
+        print_asm_template2(movzx);
 }
 
 make_EHelper(lea) {
-  operand_write(id_dest, &id_src->addr);
-  print_asm_template2(lea);
+        operand_write(id_dest, &id_src->addr);
+        print_asm_template2(lea);
 }
--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -2,61 +2,69 @@
 #include "cc.h"
 
 make_EHelper(test) {
-  TODO();
+        TODO();
 
-  print_asm_template2(test);
+        print_asm_template2(test);
 }
 
 make_EHelper(and) {
-  TODO();
+        TODO();
 
-  print_asm_template2(and);
+        print_asm_template2(and);
 }
 
 make_EHelper(xor) {
-  TODO();
+        /* PA2.1 */
+        rtl_xor(&s0, &id_dest->val, &id_src->val);
+        operand_write(id_dest, &s0);
+        rtl_update_ZFSF(&s0, id_dest -> width);
 
-  print_asm_template2(xor);
+        // CF = 0, OF = 0
+        s1 = 0;
+        rtl_set_CF(&s1);
+        rtl_set_OF(&s1);
+
+        print_asm_template2(xor);
 }
 
 make_EHelper(or) {
-  TODO();
+        TODO();
 
-  print_asm_template2(or);
+        print_asm_template2(or);
 }
 
 make_EHelper(sar) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+        TODO();
+        // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(sar);
+        print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+        TODO();
+        // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(shl);
+        print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+        TODO();
+        // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(shr);
+        print_asm_template2(shr);
 }
 
 make_EHelper(setcc) {
-  uint32_t cc = decinfo.opcode & 0xf;
+        uint32_t cc = decinfo.opcode & 0xf;
 
-  rtl_setcc(&s0, cc);
-  operand_write(id_dest, &s0);
+        rtl_setcc(&s0, cc);
+        operand_write(id_dest, &s0);
 
-  print_asm("set%s %s", get_cc_name(cc), id_dest->str);
+        print_asm("set%s %s", get_cc_name(cc), id_dest->str);
 }
 
 make_EHelper(not) {
-  TODO();
+        TODO();
 
-  print_asm_template1(not);
+        print_asm_template1(not);
 }
