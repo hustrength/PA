6b4975badda8d2fdf638ae492c45d141f6d94e63
tracer-ics2019 <tracer@njuics.org>
2021-01-03 16:14:02 +0800

    >  compile U201714733 root Linux hust-desktop 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:14:02 up  4:54,  1 user,  load average: 0.08, 0.07, 0.05 83ddf5df26ad09df68bf0780ae5cadf5d087f44

--- nemu/include/rtl11/rtl.h
+++ nemu/include/rtl11/rtl.h
@@ -159,10 +159,10 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 
 /* RTL pseudo instructions */
-/* PA2.1 */
+
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t *src1) {
     // dest <- ~src1
-    *dest = ~(*src1);
+    TODO();
 }
 
 static inline void rtl_sext(rtlreg_t *dest, const rtlreg_t *src1, int width) {
@@ -179,12 +179,12 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 
 static inline void rtl_msb(rtlreg_t *dest, const rtlreg_t *src1, int width) {
     // dest <- src1[width * 8 - 1]
-    *dest = (*src1 >> (width * 8 - 1)) & 0x1;
+    TODO();
 }
 
 static inline void rtl_mux(rtlreg_t *dest, const rtlreg_t *cond, const rtlreg_t *src1, const rtlreg_t *src2) {
     // dest <- (cond ? src1 : src2)
-    *dest = *cond ? *src1 : *src2;
+    TODO();
 }
 
 #include "isa/rtl.h"
